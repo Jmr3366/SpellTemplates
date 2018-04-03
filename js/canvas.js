610fc16a56a8d6f647048d446d36d5361fb4febc
Riley Lahd <Riley.Lahd@sjrb.ca>
2018-04-03 15:03:21 -0600

    Add mouse position tracking

--- js/canvas.js
+++ js/canvas.js
@@ -7,20 +7,34 @@ var p = 10;
 
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
+
+function getMousePos(canvas, event) {
+	var rect = canvas.getBoundingClientRect();
+	return {
+		x: event.clientX - rect.left,
+		y: event.clientY - rect.top
+	};
+}
+
 function drawBoard(){
-for (var x = 0; x <= bw; x += 40) {
-    context.moveTo(0.5 + x + p, p);
-    context.lineTo(0.5 + x + p, bh + p);
-}
+	for (var x = 0; x <= bw; x += 40) {
+		context.moveTo(0.5 + x + p, p);
+		context.lineTo(0.5 + x + p, bh + p);
+	}
 
 
-for (var x = 0; x <= bh; x += 40) {
-    context.moveTo(p, 0.5 + x + p);
-    context.lineTo(bw + p, 0.5 + x + p);
-}
+	for (var x = 0; x <= bh; x += 40) {
+		context.moveTo(p, 0.5 + x + p);
+		context.lineTo(bw + p, 0.5 + x + p);
+	}
 
-context.strokeStyle = "black";
-context.stroke();
+	context.strokeStyle = "black";
+	context.stroke();
+
+	canvas.addEventListener('click', function(evt) {
+		var mousePos = getMousePos(canvas, evt);
+		console.log(mousePos); 
+	}, false);
 }
 
 drawBoard();
