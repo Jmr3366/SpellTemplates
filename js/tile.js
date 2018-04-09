2a3c02650a699654d5b88b04aafde89fd43de172
Riley Lahd <Riley.Lahd@sjrb.ca>
2018-04-09 11:47:34 -0600

    Allow filling of a tile

--- js/tile.js
+++ js/tile.js
@@ -7,19 +7,28 @@ function Tile(x, y, size_mul, context){
 	this.covered = false;
 	this.BASE_TILE_WIDTH = 40;
 	this.BASE_TILE_HEIGHT = 40;
+	this.tile_width = this.BASE_TILE_WIDTH * size_mul;
+	this.tile_height = this.BASE_TILE_HEIGHT * size_mul;
+	this.tile_corners = [{"x":x,"y":y},{"x":x+this.tile_width,"y":y},{"x":x+this.tile_width,"y":y+this.tile_height},{"x":x,"y":y+this.tile_height}]
 
 	this.drawTile = function(){
-		var tile_width = this.BASE_TILE_WIDTH * size_mul;
-		var tile_height = this.BASE_TILE_HEIGHT * size_mul;
 		context.beginPath();
-		context.moveTo(0.5 + x, 0.5 + y);
-		context.lineTo(0.5 + x + tile_width, 0.5 + y);
-		context.lineTo(0.5 + x + tile_width, 0.5 + y + tile_height);
-		context.lineTo(0.5 + x, 0.5 + y + tile_height);
-		context.lineTo(0.5 + x, 0.5 + y);
+		//0.5 because it marks the center of the 1px wide line
+		context.moveTo(0.5 + this.tile_corners[0].x, 0.5 + this.tile_corners[0].y);
+		context.lineTo(0.5 + this.tile_corners[1].x, 0.5 + this.tile_corners[1].y);
+		context.lineTo(0.5 + this.tile_corners[2].x, 0.5 + this.tile_corners[2].y);
+		context.lineTo(0.5 + this.tile_corners[3].x, 0.5 + this.tile_corners[3].y);
+		context.lineTo(0.5 + this.tile_corners[0].x, 0.5 + this.tile_corners[0].y);
 
 		context.strokeStyle = "black";
 		context.stroke();
+	}
+
+	this.fillTile = function(fillStyle="red"){
+		context.beginPath();
+		context.rect(x+1, y+1, this.tile_width-1, this.tile_height-1);
+		context.fillStyle= fillStyle;
+		context.fill();
 	}
 
 }
